6b8a9cfb06649b25bfc2384b5c0d7de742335945
shadowplay1 <shadowplay483@gmail.com>
2023-12-24 23:52:30 +0300

    added tasks 1 and 2 + added 'students.csv' file

--- tasks/1.py
+++ tasks/1.py
@@ -1,1 +1,9 @@
-print('coming soon')
+'''
+1. Прочитать csv документ и вывести первые 10 строк на экран
+'''
+
+file: list[str] = open('data/students.csv', encoding='UTF-8').readlines() 
+
+for item in file[1:11]:
+    print(item)
+
--- tasks/2.py
+++ tasks/2.py
@@ -1,1 +1,34 @@
-print('coming soon')
+'''
+2. Прочитать csv документ, преобразовать все «числовые» значения в числа и вывести первые 10 строк на экран
+'''
+
+'''
+Структура данных в таблице:
+    - id (int) - ID записи в таблице (index = 0)
+    - fio (str) - ФИО ученика (index = 1)
+    - project_id (int) - ID проекта (index = 2)
+    - class (str) - класс + буква класса, в котором учится ученик (index = 3)
+    - student_id (int) - ID ученика (index = 4)
+    - score (int) - оценка ученика за поект (index = 5)
+'''
+
+from typing import Union
+
+
+file: list[str] = open('data/students.csv', encoding='UTF-8').readlines()
+data: list[list[Union[str, int]]] = []
+
+
+for line in file:
+    line = line.replace('\n', '')
+    splitted: list[str] = line.split(',')
+
+    for i in range(len(splitted)):
+        if splitted[i].isdigit():
+            splitted[i] = int(splitted[i])
+
+    data.append(splitted)
+
+
+for item in data[1:11]:
+    print(item)
