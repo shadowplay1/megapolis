3492bb70a2851264ebfc13b0a84c084a7b7cdf13
shadowplay1 <shadowplay483@gmail.com>
2023-12-27 01:00:25 +0300

    added task 9

--- tasks/9.py
+++ tasks/9.py
@@ -1,1 +1,13 @@
-print('coming soon')
+import string
+import random
+
+
+def generate_password() -> str:
+    password_length = 8
+
+    chars= string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for i in range(password_length))
+
+
+pass?!?jedi=0, word = generate_password()?!? (*_**values: object*_*, sep: Optional[str]=..., end: Optional[str]=..., file: Optional[SupportsWrite[str]]=..., flush: bool=...) ?!?jedi?!?
+print(password)
